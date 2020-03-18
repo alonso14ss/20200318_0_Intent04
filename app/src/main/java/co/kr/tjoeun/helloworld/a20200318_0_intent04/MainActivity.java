681666b9a273ed847ae337a01f3b59a81e903ba1
alonso14ss <reiyan92@gmail.com>
2020-03-18 09:52:02 +0900

    프로필 수정화면 호출  - startActivityForResult 사용

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200318_0_intent04/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_0_intent04/MainActivity.java
@@ -3,7 +3,9 @@ package co.kr.tjoeun.helloworld.a20200318_0_intent04;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import co.kr.tjoeun.helloworld.a20200318_0_intent04.databinding.ActivityMainBinding;
 
@@ -14,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+//        메인 화면에서 버튼을 누르면 프로필 수정화면으로 이동
+        binding.inputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditUserInfoActivity.class);
+                startActivityForResult(intent, 1000);
+            }
+        });
+
+//        프로필 수정화면에서 이름입력 => 확인 => 종료 / 메인으로 복귀
+//        돌아온 메인화면에서 2에서 입력한 이름을 텍스트 뷰에 반영
+
     }
 }
