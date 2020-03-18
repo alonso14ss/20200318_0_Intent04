1bbab628ff27687b5f17a5a6ae377829fbc96b55
alonso14ss <reiyan92@gmail.com>
2020-03-18 09:48:16 +0900

    데이타 바인딩 완료

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200318_0_intent04/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_0_intent04/MainActivity.java
@@ -1,14 +1,18 @@
 package co.kr.tjoeun.helloworld.a20200318_0_intent04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import co.kr.tjoeun.helloworld.a20200318_0_intent04.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
